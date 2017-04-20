cbe392763e57978e138259b2b25116978de71eec
wwwins <jacky.huang@isobar.com>
2017-04-20 15:25:01 +0800

    Added gradient image

--- isobar-face-detect-gevent.py
+++ isobar-face-detect-gevent.py
@@ -10,6 +10,7 @@ import requests
 import random
 import string
 import subprocess
+import math
 from ticket import ticket
 from imutils.video import VideoStream
 from skimage import io
@@ -266,9 +267,31 @@ def say_bye(contents):
     if (status==PROCESSING):
         status = END
 
+def get_gradient_image(image):
+    imgsize = image.size
+    innerColor = [67, 67, 67]
+    outerColor = [0, 0, 0]
+    for y in range(imgsize[1]):
+        for x in range(imgsize[0]):
+
+            #Find the distance to the center
+            distanceToCenter = math.sqrt((x - imgsize[0]/2) ** 2 + (y - imgsize[1]/2) ** 2)
+            #Make it on a scale from 0 to 1
+            distanceToCenter = float(distanceToCenter) / (math.sqrt(2) * imgsize[0]/2)
+            #Calculate r, g, and b values
+            r = outerColor[0] * distanceToCenter + innerColor[0] * (1 - distanceToCenter)
+            g = outerColor[1] * distanceToCenter + innerColor[1] * (1 - distanceToCenter)
+            b = outerColor[2] * distanceToCenter + innerColor[2] * (1 - distanceToCenter)
+
+            #Place the pixel
+            image.putpixel((x, y), (int(r), int(g), int(b)))
+    return image
+
 def get_image_text(contents):
 
     image = Image.new("RGB",(600,400),(0,0,0))
+
+    image = get_gradient_image(image)
     draw = ImageDraw.Draw(image)
     text = contents
 
